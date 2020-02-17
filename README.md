d759ee6e6a7947a5866d12d0a98e9220c6e6c766
Weixing Nie <wn5@uw.edu>
2020-02-16 16:57:40 -0800

    I did the map

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # geo-narrative
+
+**Meeting:** Wednesdays, 2:30 - 5:20, SMI 109
+
+**Instructor:** Bo Zhao, SMI 416B, Office hours by appointment
+
+**Contact:** 206.685.3846, zhaobo@uw.edu, jakobzhao `skype/wechat`
+
+**Student:** Weixing Nie / Oliver Nie
+
+**Keywords:**
+  - Hermeneutics, Inductive visualization
+  - Geo-narrative, web mapping, storymap
+
+## Goal of the storymap
+I created this map because I want to demonstrate the spatial and physical spread of the coronavirus.
+This map take in two parts as important for the map visualization. I tried to combine the subjective fact and objective opinion. I demonstrate the origin, the spread of coronavirus but I also expressed my personal concern for our current health care / medical system. I begin with the story in China, then I draw the connection / the spread all the way to Japan. Especially, the case found in Ginza, which is considered as one of the most dangerous virus appearing at the most popular tourists favored place. Such a future remains unknown but I am deeply worried about the Japanese government or the future of our effort against coronavirus. 
---
+++ js/config.js
@@ -0,0 +1,108 @@
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1Ijoid2VpeGluZ25pZSIsImEiOiJjazZqdTZ1MmIwMWZ1M2xwOGcyejZ5M2hmIn0.UTnQ8LMD7rkcL4eTm0iAsg',
+    showMarkers: true,
+    alignment: 'right',
+    title: 'The spread of coronavirus',
+    subtitle: 'An objective map',
+    byline: 'Weixing Nie, 2/16/2020, Jan.2020 --- Feb.2020, Asia',
+    footer: 'References, acknowledgments, footnotes',
+    chapters: [
+        {
+            id: 'The possible origin',
+            title: 'The outbreak of Coronavirus',
+            image: 'img/Wuhan_market.jpg',
+            description: 'An ongoing outbreak of coronavirus disease 2019 (COVID-19) caused by the 2019 novel coronavirus (SARS-CoV-2) started in December 2019. It was first identified in Wuhan, capital of Hubei province in China, after 41 people presented with pneumonia of no clear cause. The "pneumonia of unknown cause" was suspected linking to Huanan Seafood Wholesale Market, which had a thousand stores selling bats, snakes, chickens, and other wild animals without health procation.',
+            location: {
+                center: [114.25666, 30.62051],
+                zoom: 16.41,
+                pitch: 56.50,
+                bearing: -0.02
+            },
+            onChapterEnter: [
+                {
+                    layer: 'phl-city-limits',
+                    opacity: .45
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'phl-city-limits',
+                    opacity: 0.1
+                }
+            ]
+        },
+        {
+            id: 'The spread in China',
+            title: 'Coronavirus outbreak in China',
+            image: 'img/China.jpg',
+            description: 'In just a few weeks, the coronavirus had spread across proviences. The cirtical condition in China had turned more serious and intense than SARS.',
+            location: {
+                center: [111.03403, 36.56962],
+                zoom: 2.86,
+                pitch: 3.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+              {
+                  layer: 'belmont',
+                  opacity: 1
+              }
+            ],
+            onChapterExit: [
+              {
+                  layer: 'belmont',
+                  opacity: 1
+              }
+            ]
+        },
+        {
+            id: 'Japan',
+            title: 'Across the ocean, Coronavirus detected in Japan',
+            image: 'img/Japan.jpg',
+            description: 'On a cruise ship quarantined near Tokyo, it was reported for the first case of the coronavirus outbreak. So far, there are already 44 or more confirming cases in Japan.',
+            location: {
+                center: [139.19140, 40.26337],
+                zoom: 4.03,
+                pitch: 3.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+              {
+                  layer: 'belmont',
+                  opacity: 1
+              }
+            ],
+            onChapterExit: [
+              {
+                  layer: 'belmont',
+                  opacity: 1
+              }
+            ]
+        },
+        {
+            id: 'Ginza',
+            title: 'One confirmed case in Ginza',
+            image: 'img/Ginza.jpg',
+            description: 'The patients with positive coronavirus test was once spoted on the railroad station at Ginza in Tokyo. Ginza is one of the crowdest places in the world for tourists, which concerns a lot of people of a futher spread on coronavirus.',
+            location: {
+                center: [139.73676, 35.75746],
+                zoom: 14.92,
+                pitch: 60.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'belmont',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'belmont',
+                    opacity: 0
+                }
+            ]
+        }
+    ]
+};
